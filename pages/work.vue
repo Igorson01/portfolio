3910f1c205d421f3367a12386ecb4a52f73e6873
Igorson01 <josefkrolewski@gmail.com>
2023-09-06 13:53:03 +0200

    all

--- pages/work.vue
+++ pages/work.vue
@@ -3,7 +3,7 @@
         <h2 class="text-white font-exo text-5xl opacity-0" ref="title" style="transform: translateY(30px);">MY WORK</h2>
         <div class="flex space-x-4">
             <div ref="projects" v-for="project in projects" class="w-full opacity-0" style="transform: translateX(30px);">
-                <a href="https://igorson01.github.io/3dgierson/">
+                <a :href="project.link">
         <img :src="project.image.url" alt="FightGame" style="height: 70vh;" class="object-cover">
         <p class="text-white font-space-mono text-sm">{{ project.title }}</p>
                 </a>
@@ -25,25 +25,29 @@ export default{
              image:{
                 url: fightgameImg
              },
-             title: 'Fight Game'
+             title: 'Fight Game',
+             link:'https://64f864bbdd155770de5a8338--dapper-sundae-da5b6e.netlify.app/'
             },
             {
              image:{
                 url: kulkigameImg
              },
-             title: 'Kulki Game'
+             title: 'Kulki Game',
+             link:'https://64f867454c94e174a2e0e56f--extraordinary-cheesecake-36b642.netlify.app/'
             },
             {
              image:{
                 url: kinggameImg
              },
-             title: 'King Game'
+             title: 'King Game',
+             link: 'https://64f8679dbe2880747c0bdd7c--fluffy-bienenstitch-57d48a.netlify.app/'
             },
             {
              image:{
                 url: spaceinvadergameImg
              },
-             title: 'Space Invader Game'
+             title: 'Space Invader Game',
+             link:'https://64f867e8dd155773f35a81e5--subtle-paprenjak-b331f0.netlify.app/'
             },
             {
              image:{
